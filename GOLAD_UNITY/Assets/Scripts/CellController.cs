f55dbbe6d6cc96af2c1a158f9235d14c83e65f6a
vv-b-s <valentin.v1997@gmail.com>
2017-08-23 17:24:55 +0300

    Added unity version of GOLAD

---
+++ GOLAD_UNITY/Assets/Scripts/CellController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CellController : MonoBehaviour
+{
+    public int GridHeight, GridWidth;
+    private static GameObject[,] CellGrid;
+    public GameObject LivingCell;
+    public GameObject DeadCell;
+
+    public Transform BoardHolder;
+
+    /// <summary>
+    /// Decide where to put living cells
+    /// </summary>
+    /// <param name="grid"></param>
+    public void GenerateCells()
+    {
+        CellGrid = new GameObject[GridHeight, GridWidth];
+        for (int i = 0; i < GridHeight; i++)
+            for (int j = 0; j < GridWidth; j++)
+                CellGrid[i, j] = Random.Range(0,2) == 1 ? LivingCell : DeadCell;
+            
+    }
+
+    /// <summary>
+    /// Displays the cells in the game
+    /// </summary>
+    public void DrawCells()
+    {
+        GameObject Board = GameObject.Find("Board");
+        if (Board != null)
+            Destroy(Board);
+        BoardHolder = new GameObject("Board").transform;
+
+        for (int i = 0; i < CellGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < CellGrid.GetLength(1); j++)
+            {
+                var instance = Instantiate(CellGrid[i, j], new Vector2(i, j), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(BoardHolder);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the generation
+    /// </summary>
+    public void UpdateGrid()
+    {
+        GameObject[,] newGrid = new GameObject[CellGrid.GetLength(0), CellGrid.GetLength(1)];
+        for (int i = 0; i < CellGrid.GetLength(0); i++)
+            for (int j = 0; j < CellGrid.GetLength(1); j++)
+            {
+                int neighbourCount = GetNeighbourCount(CellGrid[i, j],new int[] { i, j });
+
+                if (CellGrid[i, j].tag=="LivingCell" && (neighbourCount < 2 || neighbourCount > 3))
+                    newGrid[i, j] = DeadCell;
+
+                else if (CellGrid[i, j].tag=="DeadCell" && neighbourCount == 3)
+                    newGrid[i, j] = LivingCell;
+
+                else
+                    newGrid[i, j] = CellGrid[i, j];
+            }
+        CellGrid = newGrid;
+    }
+
+    /// <summary>
+    /// Gets the number of neighbours around a cell
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <returns></returns>
+    private int GetNeighbourCount(GameObject cell, int[] gridCoordinates)
+    {
+        int neighbourCount = 0;
+        for (int i = gridCoordinates[0] - 1; i <= gridCoordinates[0] + 1; i++)
+            for (int j = gridCoordinates[1] - 1; j <= gridCoordinates[1] + 1; j++)
+            {
+                if (i < 0 || j < 0 || i > CellGrid.GetLength(0) - 1 || j > CellGrid.GetLength(1) - 1)
+                    continue;
+
+                if (!(i==gridCoordinates[0]&&j==gridCoordinates[1]) && CellGrid[i, j].tag == "LivingCell") 
+                    neighbourCount++;
+            }
+        return neighbourCount;
+    }
+}	
---
+++ GOLAD_UNITY/Assets/Scripts/CellInstantiator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellInstantiator : MonoBehaviour {
+
+    public CellController Life;
+    private float time;
+	void Start ()
+    {
+        Life = GetComponent<CellController>();      // Instantiate Cell Controller
+
+        Life.GenerateCells();
+        Life.DrawCells();
+        time = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var newTime = Time.time;
+        if(newTime!=time)
+        {
+            time = newTime;
+            Life.UpdateGrid();
+            Life.DrawCells();
+        }
+	}
+}
